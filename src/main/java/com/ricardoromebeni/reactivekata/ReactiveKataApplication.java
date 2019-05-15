c860ce14cb6c1362741384fa0124badd614870c8
RicardoRB <ricardoromebeni@gmail.com>
2019-05-15 18:51:56 +0200

    Created project.

---
+++ src/main/java/com/ricardoromebeni/reactivekata/ReactiveKataApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoromebeni.reactivekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveKataApplication.class, args);
+	}
+
+}
